6b03d1743af0183a108358b4c22d777c7a13bcf4
Lilli Szafranski <lilli@daoofdev.com>
2016-02-12 16:33:13 -0800

    [WDA-2] Pulled out the http data fetching into its own class for better encapsulation.
    Made a DataFetcher class to grab data from a URL. Made it its own class primarily for two reasons: a) Easier to test against a multitude of urls, response types, data, etc. and b) so that I could make it generic so that it can fetch the json weather data and icons, etc.

--- app/src/main/java/com/daoofdev/weatherday/MainActivity.java
+++ app/src/main/java/com/daoofdev/weatherday/MainActivity.java
@@ -22,6 +22,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
 {
